b42952e82b8ca8c58d50169f8100c9a9cc551f95
mareike.janiak@computationalgenomics.ca <mareike.janiak@computationalgenomics.ca>
2023-05-08 15:23:03 -0400

    demuxmetrics : revert changes to DemuxFastqs

--- multiqc_c3g/modules/c3g_demuxmetrics/DemuxFastqs.py
+++ multiqc_c3g/modules/c3g_demuxmetrics/DemuxFastqs.py
@@ -19,15 +19,13 @@ def parse_reports(self):
     ## Parse fgbio DemuFastqs output files
     self.barcode_data = dict()
     self.unexpected_per_lane = dict()
-    self.unexpected_barcode_data = dict()
     report_found = []
 
-    for f in self.find_log_files("c3g_demuxmetrics/demuxfastqs"): ## stuck on how the log files are found and read in, but realizing now that maybe I need a separate script for each log file? So instead of adding to this script, I should have started a fresh one?
+    for f in self.find_log_files("c3g_demuxmetrics/demuxfastqs"): 
         lane_data = expected_metrics(self, f)
         lane = self.get_lane(f)
         self.unexpected_per_lane[f"L{lane}"] = unexpected_metrics(f)
         self.barcode_data = {**self.barcode_data, **lane_data}
-        self.unexpected_barcode_data = {**self.unexpected_barcode_data, **lane_data} ### ??? Not sure this is right
         report_found.append(f['fn'])
 
     if report_found:
@@ -40,11 +38,6 @@ def parse_reports(self):
             name = "Barcodes - Lane Overview",
             description = "Overview of number of clusters assigned to expected barcodes.",
             plot = lane_overview_table(self)
-        )
-        self.add_section(
-            name = "Barcodes - Unexpected",
-            description = "The counts for unexpected barcodes and their potential matches to sequencing barcodes are shown below. Note that only frequent barcodes are shown (>50000 reads)."
-            plot = unexpected_barcodes_table(self)
         )
 
     return len(report_found)
@@ -100,17 +93,6 @@ def get_clean_barcode_name(barcode, library):
     else:
         barcode = raw_barcode
     return barcode
-
-def matched_metrics(self, f):
-    metrics = ()
-
-    buff = StringIO(f['f'])
-    reader = csv.DictReader(buff, delimiter="\t")
-    for row in reader:
-        barcode = row.pop('Sequence')
-        ###??? 
-
-    return metrics
 
 def expected_barcodes_table(self):
     headers = OrderedDict()
@@ -195,22 +177,3 @@ def lane_overview_table(self):
         'format': '{:,.1f}',
     }
     return table.plot(self.unexpected_per_lane, headers, {"col1_header": "Lane"})
-
-def unexpected_barcodes_table(self):
-    largest_value = max([y['ReadCount'] for (_,y) in self.unexpected_barcode_data.items()])
-    headers = OrderedDict()
-    headers['ReadCount'] = {
-            'title' : "Number of reads",
-            'description' : "number of reads for this barcode in run",
-            'format' : '{:,.0f}',
-            'max' : largest_value
-            }
-    headers['Sequence'] = {
-            'title' : "Barcode sequence",
-            'description' : "sequence of undetermined barcode"
-            }
-    headers['Matches'] = {
-            'title' : "Matches",
-            'description' : "sequence and names of any matches to the undetermined barcode in database of sequencing barcodes"
-            }
-    return table.plot(self.unexpected_barcode_data, headers, {"col1_header": "Lane"})

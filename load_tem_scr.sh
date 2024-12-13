0e51d46ca401a2bf86294dcdf3358ad5156eed8c
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2024-12-13 13:39:09 -0500

    update install script

--- load_tem_scr.sh
+++ load_tem_scr.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mkdir -p $MUGQIC_INSTALL_HOME_DEV/software/MultiQC/MultiQC_C3G-1.0-beta
-module load python/3.8.10
-pip install --prefix=$MUGQIC_INSTALL_HOME_DEV/software/MultiQC/MultiQC_C3G-1.0-beta -U multiqc git+file:///home/newtonma/apps/multiqc_c3g
-#pip install --prefix=$MUGQIC_INSTALL_HOME_DEV/software/MultiQC/MultiQC_C3G-1.0-beta multiqc file:///home/newtonma/apps/multiqc_c3g
-#pip install --prefix=$MUGQIC_INSTALL_HOME_DEV/software/MultiQC/MultiQC_C3G-1.0-beta multiqc git+https://newtonma@bitbucket.org/mugqic/multiqc_c3g.git@newton_c3g
+version=1.23 # version of MultiQC to use
+commit=8ade80c # commit in MultiQC_c3g repo
 
+mkdir -p $MUGQIC_INSTALL_HOME_DEV/software/MultiQC_C3G/MultiQC_C3G-${version}_${commit}
+
+git clone git@github.com:c3g/MultiQC_c3g.git -b ${commit} $MUGQIC_INSTALL_HOME_DEV/software/MultiQC_C3G/MultiQC_C3G-${commit}
+
+module purge
+module load python/3.12.2
+pip install --prefix=$MUGQIC_INSTALL_HOME_DEV/software/MultiQC/MultiQC_C3G-${version}_${commit} -U multiqc==${version} git+file:$MUGQIC_INSTALL_HOME_DEV/software/MultiQC_C3G/MultiQC_C3G-${commit}

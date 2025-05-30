85273dbf529e0432473d6cc38d62f123264b60d6
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2025-05-30 10:45:23 -0400

    change to older version of bclconvert, try to debug clean_s_name error

--- multiqc_c3g/runprocessing_base.py
+++ multiqc_c3g/runprocessing_base.py
@@ -30,8 +30,8 @@ class RunProcessingBaseModule(BaseMultiqcModule):
     def get_lane(self, f):
         return self.get_lane_from_path(f['root'])
 
-    def clean_s_name(self, s_name, f=None, root=None, filename=None, search_pattern_key=None, lane=None):
-        s_name = super().clean_s_name(s_name, f, root, filename, search_pattern_key)
+    def clean_s_name(self, s_name, f=None, root=None, filename=None, search_pattern_key=None, fn_clean_exts = None, fn_clean_trim = None, prepend_dirs = None, lane=None):
+        s_name = super().clean_s_name(s_name, f, root, filename, search_pattern_key, fn_clean_exts, fn_clean_trim, prepend_dirs)
         if lane:
             return "L{} | {}".format(lane, s_name)
         lane_from_path = self.get_lane(f)
